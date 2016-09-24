45acdcc6af02e59f4c0ad8f1ef485066bde440bf
Michael Stewart <mike@MBPr.local>
2016-09-24 09:02:27 -0400

    Added grid recoloring functionality

---
+++ scripts/etch.js
@@ -0,0 +1,21 @@
+rows = 16;
+cols = 16;
+
+$(document).ready(function() {
+  var $gridholder = $("#gridholder");
+  for (var y = 0; y < rows; y++) {
+    var $gridrow = $("<div class='gridrow'></div>");
+    $gridrow.attr("id", "row" + y);
+    $gridholder.append($gridrow);
+    for (var x = 0; x < cols; x++) {
+      var $cell = $("<div class='cell'></div>");
+      $cell.attr("id", "cell" + (cols * y + x));
+      $gridrow.append($cell);
+    }
+  }
+
+  $('.cell').click(function() {
+    $(this).css("background-color", "black");
+  });
+
+});
