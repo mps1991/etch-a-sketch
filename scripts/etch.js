fd8d393fd6ad5731fd5739fac641544d045ea30a
Michael Stewart <mike@MBPr.local>
2016-09-24 11:31:29 -0400

    attempting to add button

--- scripts/etch.js
+++ scripts/etch.js
@@ -2,16 +2,18 @@ rows = 16;
 cols = 16;
 
 $(document).ready(function() {
-	makeGrid(rows, cols);
+	makeGrid(30, 30);
 
   $('.cell').hover(function() {
     $(this).animate({"background-color": "black"}, 400);
   });
-
 });
 
-function makeGrid() {
+function makeGrid(rows, cols) {
 	var $gridholder = $("#gridholder");
+  $gridholder.css("width", 21 * rows + 1);
+  $gridholder.css("height", 21 * cols + 1);
+
   for (var y = 0; y < rows; y++) {
     var $gridrow = $("<div class='gridrow'></div>");
     $gridrow.attr("id", "row" + y);
@@ -22,4 +24,9 @@ function makeGrid() {
       $gridrow.append($cell);
     }
   }
+}
+
+function clear() {
+  var side = prompt("Please enter new side length", 16);
+  makeGrid(side, side);
 };
