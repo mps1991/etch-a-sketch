c3eee5fd104ed411080cba809e8bd928bde715ae
Michael Stewart <mike@MBPr.local>
2016-09-24 16:03:37 -0400

    Revert "Implemented clear button"
    This reverts commit df50135deadd78acd4a0462934bed89478ac6929.

--- scripts/etch.js
+++ scripts/etch.js
@@ -1,33 +1,29 @@
-$(document).ready(function() {
-	makeGrid(16);
+rows = 16;
+cols = 16;
 
-	$("#clearbutton").click(function() {
-		makeGrid(prompt("Enter side length", 16));
-		$(document).ready();
-	});
+$(document).ready(function() {
+	makeGrid(30, 30);
+
+  $('.cell').hover(function() {
+    $(this).animate({"background-color": "black"}, 400);
+  });
 });
 
-function makeGrid(size) {
+function makeGrid(rows, cols) {
 	var $gridholder = $("#gridholder");
-	$gridholder.empty();
-  $gridholder.css("width", 11 * size + 1);
-  $gridholder.css("height", 11 * size + 1);
-	console.log($(".cell").width());
+  $gridholder.css("width", 21 * rows + 1);
+  $gridholder.css("height", 21 * cols + 1);
 
-  for (var y = 0; y < size; y++) {
+  for (var y = 0; y < rows; y++) {
     var $gridrow = $("<div class='gridrow'></div>");
     $gridrow.attr("id", "row" + y);
     $gridholder.append($gridrow);
-    for (var x = 0; x < size; x++) {
+    for (var x = 0; x < cols; x++) {
       var $cell = $("<div class='cell'></div>");
-      $cell.attr("id", "cell" + (size * y + x));
+      $cell.attr("id", "cell" + (cols * y + x));
       $gridrow.append($cell);
     }
   }
-
-	$('.cell').hover(function() {
-    $(this).animate({"background-color": "black"}, 400);
-  });
 }
 
 function clear() {
